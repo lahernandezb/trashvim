97aac9802fed73841d3e3f057e30c069245f6ef1
Luis Hernandez <luis.hernandez@salesforce.com>
2023-07-26 06:58:04 -0400

    fix: git signs symbols

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-
-local keymap = vim.api.nvim_set_keymap
-
 if vim.g.vscode then
+  require("config.hlslens")
+  local keymap = vim.api.nvim_set_keymap
+
   local function v_notify(cmd)
     return string.format("<cmd>call VSCodeNotifyVisual('%s', 1)<CR>", cmd)
   end
@@ -10,7 +10,7 @@ if vim.g.vscode then
     return string.format("<cmd>call VSCodeNotify('%s')<CR>", cmd)
   end
 
-  keymap('n', '<leader>', notify 'whichkey.show', { noremap = true, silent = true})
+  keymap('n', '<leader>', notify 'whichkey.show', { noremap = true, silent = true })
   keymap('n', 'gr', notify 'references-view.find', { noremap = true, silent = true })
   keymap('n', 'gR', notify 'references-view.findImplementations', { noremap = true, silent = true })
   --[[ keymap('n', 'gO', notify 'outline.focus', { noremap = true, silent = true }) ]]
